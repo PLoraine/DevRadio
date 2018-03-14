533005d0f106f79220b4f29e674a77c17391107c
Lukas Lepez <33353776+LukasLepez@users.noreply.github.com>
2018-03-14 21:24:14 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ var cookies = localStorage.getItem("cookies");
 
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = $('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 var id;
